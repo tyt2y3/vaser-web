7d80219ab265064c5d453103c1b40cbd173ca1c6
tyt2y3 <tyt2y3@gmail.com>
2013-07-22 22:55:50 +0800

    web

---
+++ back.js
@@ -0,0 +1,35 @@
+/*
+(function()
+{	//init
+	var tips_list = document.getElementsByClassName('box_tips');
+	for ( var i=0; i<tips_list.length; i++)
+	{
+		var a_list = tips_list[i].getElementsByTagName('a');
+		if ( a_list.length > 0)
+		{
+			var instr = a_list[0].innerHTML;
+			if ( instr.length==4)
+				if ( instr=='hide')
+					box_tips_switchsize( a_list[0]);
+		}
+	}
+}());*/
+function box_tips_switchsize(obj)
+{
+	if ( obj.innerHTML=='show')
+	{	//show the box tips
+		obj.parentNode.style.height='auto';
+		obj.innerHTML='hide';
+	}
+	else
+	{	//hide it
+		obj.parentNode.style.height='140px';
+		obj.innerHTML='show';
+	}
+}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-37320960-7', 'tyt2y3.github.io');
+ga('send', 'pageview');
